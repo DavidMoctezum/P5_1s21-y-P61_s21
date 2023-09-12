3563efb4127324e2b77d81e11f5a720c92ef5df3
David Moctezuma Pérez <144501989+DavidMoctezum@users.noreply.github.com>
2023-09-12 16:01:54 -0600

    p5_1s21 y p6_1s21

---
+++ p6_1s21.java
@@ -0,0 +1,12 @@
+class p6_1s21{
+	public static void main(String[] args) {
+		System.out.println("Escribe tu nombre"); 
+		String nombre; 
+	   	String edad;
+	   	nombre = "David";  
+	   	edad="23";
+		System.out.println("Tu nombre es " + nombre); 
+		System.out.println("La edad es " + edad); 
+		System.out.println("Puede proseguir programando."); 
+	}
+}
